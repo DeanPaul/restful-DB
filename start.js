28766be6de7b9c99b82ec3e9ce29fab41e99f601
paul <35568588+DeanPaul@users.noreply.github.com>
2018-03-19 19:15:24 +0800

    Create start.js

---
+++ start.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.get('/', (request, response) => response.send('Hello World'));
+
+app.use('/api', router);
+
+router.get('/', (request, response) => {
+  response.json({message: 'Hello, welcome to my server'});
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+var tableManagement = require('./tableManagement');
+var dataManagement = require('./dataManagement');
+
+tableManagement.init(router);
+dataManagement.init(router);
